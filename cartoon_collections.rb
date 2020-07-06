073ad64026b99cb91b1839df1e9bb3e7b90ccc34
congenial-course-2234 <jakelozano94@gmail.com>
2020-07-06 05:52:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,8 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index do |character|
+  array.each_with_index do |character, index|
+    puts 
     
   # Use `each_with_index` to enumerate over the provided array
   #
